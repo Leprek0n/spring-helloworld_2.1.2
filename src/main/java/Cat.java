ac2d400463fcd89713045cbdf5591969b7f0376e
Leprek0n <fhrhrhhrueuu@gmail.com>
2021-07-18 10:27:08 +0600

    init

---
+++ src/main/java/Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+    private String message;
+    public void setMessage(String s) {
+        this.message = s;
+    }
+    public String getMessage(){
+        return message;
+    }
+}
